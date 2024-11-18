a95e720c687e68354d88744d004c6aea7e045ac1
IQRAKHAN2998 <iqrakhan2998@gmail.com>
2024-11-19 01:54:51 +0500

    animate changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <div  className= " bg-pink-200 max-w-full max-h-screen flex justify-around flex-col sm:flex-row items-center md:gap-72">
           {/* leftside */}
           <div className=" pt-20 pl-10 font-bold text-black text-2xl sm:text-4xl flex-1">
-            <h1 className=" ">  Hi, I am IQRA KHAN</h1>
-            <h2 className="text-3lg sm:text-4xl "> I am a Front-end Developer</h2>
+            <h1 className="animate-pulse ">  Hi, I am IQRA KHAN</h1>
+            <h2 className="text-3lg sm:text-4xl animate-pulse "> I am a Front-end Developer</h2>
 
             <div className="my-5 ">
 
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -4,8 +4,8 @@ import Wrapper from '../../components/wrapper/page';
 export default function Projects() {
   return (
     <Wrapper>
-      <h1 className="text-3xl underline font-bold text-center p-5">My-Projects</h1>
-      <div className=' text-white bg-slate-700 pt-10 mt-20 border-2 rounded-lg'>
+      <h1 className="text-4xl underline font-bold text-center p-5 animate-bounce">My-Projects</h1>
+      <div className=' text-white bg-slate-700 pt-10 mt-0 border-2 rounded-lg'>
         <h1 className='text-center font-bold text-5xl'>Hackathon-1</h1>
 
         <p className='text-center  font-bold text-3xl py-5'>Quarter-2</p>
