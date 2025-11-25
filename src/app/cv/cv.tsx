4bf3f1279c9582ba30488587efb8102c7470abf9
IQRAKHAN2998 <iqrakhan2998@gmail.com>
2025-11-26 00:53:31 +0500

    first commit

--- src/app/cv/cv.tsx
+++ src/app/cv/cv.tsx
@@ -5,7 +5,11 @@ import Button from "../../components/Button/page"
 const Cv = () => {
   return (
     <div >
-        <a href="document/iqrakhan_cv.pdf" target="_blank">
+        {/* <a href="document/iqrakhan_cv.pdf" target="_blank"> */}
+        <a href="document/iqrakhan_agentic_ai.pdf" target="_blank">
+
+
+        
         {/* <a href="/page.tsx" target="_blank"> */}
         
         <Button text="Download CV"></Button> </a>
